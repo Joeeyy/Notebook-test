e870710d3a19191e0cdfaf28f02cc0e5a04ce6df
Joeeyy <joeeeee@foxmail.com>
2019-08-06 15:49:18 +0800

    Track 1 files into repository.
    - modified codes/java_multi_thread/ThreadTest1.java
    
    Auto commit by GitBook Editor

--- codes/java_multi_thread/ThreadTest1.java
+++ codes/java_multi_thread/ThreadTest1.java
@@ -10,19 +10,34 @@ class RunnableDemo implements Runnable{
     }
 
     public void run(){
-
+        System.out.println("Running thread with name: " + this.threadName);
+        try{
+            for (int i=0;i<5;i++){
+                System.out.println("Thread: " + this.threadName + ", " + i);
+            }
+        }
+        catch (InterruptedException e){
+            System.out.println("Thread " + this.threadName + " interrupted.");
+        }
+        System.out.println("Exiting thread with name: " + this.threadName);
     }
 
     public void start(){
         System.out.println("Start thread with name: " + this.threadName);
         if (this.t == null){
             t = new Thread(this, this.threadName);
-            t.start()
+            t.start();
         }
     }
 }
 
 
 class ThreadTest1 {
+    public static void main(String args[]){
+        RunnableDemo rd1 = new RunnableDemo("aaa");
+        RunnableDemo rd2 = new RunnableDemo("bbb");
 
+        rd1.start();
+        rd2.start();
+    }
 }
