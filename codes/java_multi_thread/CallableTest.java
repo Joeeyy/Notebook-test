489ebd3bed375d629caac245ef9271d99232c2fe
Joeeyy <joeeeee@foxmail.com>
2019-08-06 16:33:52 +0800

    Track 3 files into repository.
    - modified codes/java_multi_thread/CallableTest.java
    - untracked codes/java_multi_thread/CallableDemo.class
    - untracked codes/java_multi_thread/CallableTest.class
    
    Auto commit by GitBook Editor

--- codes/java_multi_thread/CallableTest.java
+++ codes/java_multi_thread/CallableTest.java
@@ -1,11 +1,36 @@
 import java.util.*;
+import java.util.concurrent.*;
 
-class CallableDemo implements Callble<Integer> {
-
+class CallableDemo implements Callable<Integer> {
+    public Integer call() throws Exception {
+        int i = 0;
+        for (;i<100;i++){
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+        return i;
+    }
 }
 
 class CallableTest {
     public static void main(String args[]){
-        
+        CallableDemo cd = new CallableDemo();
+        FutureTask ft = new FutureTask<>(cd);
+
+        for (int i=0;i<100;i++){
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+            if (i == 20){
+                new Thread(ft, "threadWithReturnedValue").start();
+            }
+        }
+
+        try{
+            System.out.println("subThread: " + ft.get());
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        catch (ExecutionException e){
+            e.printStackTrace();
+        }
     }
 }
