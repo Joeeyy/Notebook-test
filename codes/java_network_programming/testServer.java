4e9c4cf2e3f1e0a9f67ce9bb63a19e41acef86a5
Joeeyy <joeeeee@foxmail.com>
2019-08-07 13:21:22 +0800

    Track 1 files into repository.
    - modified codes/java_network_programming/testServer.java
    
    Auto commit by GitBook Editor

--- codes/java_network_programming/testServer.java
+++ codes/java_network_programming/testServer.java
@@ -2,6 +2,35 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 
-class testServer {
+class testServer extends Thread{
+    private ServerSocket serverSocket;
 
+    public testServer(int port) throws IOException{
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+    }
+
+    public void run(){
+        System.out.println("Waiting for connection: " + serverSocket.getLocalPort());
+
+    }
+    catch (SocketTimeoutException e){
+        System.out.println("Server timeout.");
+        break;
+    }
+    catch (IOException e){
+        e.printStackTrace();
+        break;
+    }
+
+    public static void main(String args[]){
+        int port = Integer.parseInt(args[0]);
+        try {
+            Thread t = new testServer(port);
+            t.run();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
