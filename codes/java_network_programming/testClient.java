b02e8cbcb3067d0315109a390b6a2c3d40d8a5ef
Joeeyy <joeeeee@foxmail.com>
2019-08-07 13:16:38 +0800

    Track 1 files into repository.
    - modified codes/java_network_programming/testClient.java
    
    Auto commit by GitBook Editor

--- codes/java_network_programming/testClient.java
+++ codes/java_network_programming/testClient.java
@@ -1,6 +1,27 @@
 import java.util.*;
 
 class testClient {
-    
+    public static void main(String args[]){
+        String serverName = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try{
+            System.out.println("Connecting to Host " + serverName + " port " + port);
+            Socket client = new Socket(serverName, port);
+            System.out.println("Name of remote server: " + client.getRemoteSocketAddress());
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+
+            out.writeUTF("hello to server from " + client.getLocalSocketAddress());
+            InputStream inFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+            System.out.println("Response from server: " + in.readUTF());
+
+            client.close()
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
 
